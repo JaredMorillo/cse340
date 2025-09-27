c935ab1c8902415a0b7b6c26f5b8f34d8104e8e1
JaredMorillo <jared.morillo@hotmail.com>
2025-09-27 13:49:19 -0500

    Add Classification and Inventory

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -40,7 +40,72 @@ async function getVehicleByInventoryId(inventory_id) {
     console.error("getVehicleDetailsByInventoryId" + error)
   }
 }
+//This function will return all rows from the inventory table where classificiation if matches with the parameter.
+//Example: if 3 cars have the same SUV type, I will return them 3 if their classificationId matches with the parameter.
 
+/* *****************************
+*   Register new classification
+* *************************** */
+async function registerClassification(classification_name) {
+  try {
+    const sql = "INSERT INTO classification (classification_name) VALUES ($1)"
+    return await pool.query(sql, [classification_name]) 
+  } catch (error) {
+    console.error("registerClassification" + error)
+  }
+}
 
+/* **********************************
+*   Check for existing classification
+* *********************************** */
 
-module.exports = {getClassifications, getInventoryByClassificationId, getVehicleByInventoryId};
+async function checkExistingClassification(classification_name){
+  try {
+    const sql = "SELECT * FROM classification WHERE classification_name = $1"
+    const classification = await pool.query(sql, [classification_name])
+    return classification.rowCount
+  } catch (error) {
+    console.error("checkExistingClassification" + error)
+  }
+}
+
+/* *****************************
+*   Register new Inventory Element
+* *************************** */
+async function registerInventory(inv_make, inv_model, inv_year, inv_description, inv_image, inv_thumbnail, inv_price, inv_miles, inv_color, classification_id) {
+  try {
+    const sql = "INSERT INTO inventory (inv_make, inv_model, inv_year, inv_description, inv_image, inv_thumbnail, inv_price, inv_miles, inv_color, classification_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)"
+    return await pool.query(sql, [inv_make, inv_model, inv_year, inv_description, inv_image, inv_thumbnail, inv_price, inv_miles, inv_color, classification_id]) 
+  } catch (error) {
+    console.error("registerInventory" + error)
+  }
+}
+
+/* *****************************
+*   Update Inventory Element
+* *************************** */
+async function updateInventory(inv_id, inv_make, inv_model, inv_year, inv_description, inv_image, inv_thumbnail, inv_price, inv_miles, inv_color, classification_id) {
+  try {
+    const sql = "UPDATE public.inventory SET inv_make = $1, inv_model = $2, inv_description = $3, inv_image = $4, inv_thumbnail = $5, inv_price = $6, inv_year = $7, inv_miles = $8, inv_color = $9, classification_id = $10 WHERE inv_id = $11 RETURNING *"
+    const data = await pool.query(sql, [inv_make, inv_model, inv_description, inv_image, inv_thumbnail, inv_price, inv_year, inv_miles, inv_color, classification_id, inv_id]) 
+    return data.rows[0]
+
+  } catch (error) {
+    console.error("model error:" + error)
+  }
+}
+
+/* *****************************
+*   Delete Inventory Element
+* *************************** */
+async function deleteVehicleByInventoryId(inv_id) {
+  try {
+    const sql = 'DELETE FROM inventory WHERE inv_id = $1'
+    const data = await pool.query(sql, [inv_id])
+  return data
+  } catch (error) {
+    new Error("Delete Inventory Error")
+  }
+}
+
+module.exports = { getClassifications, getInventoryByClassificationId, getVehicleByInventoryId, registerClassification, checkExistingClassification, registerInventory, updateInventory, deleteVehicleByInventoryId }; //We export it as what is returned will be handled by a utility.
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -3,12 +3,59 @@ const express = require("express")
 const router = new express.Router() 
 const invController = require("../controllers/invController")
 const utilities = require("../utilities")
+const mgmtValidate = require('../utilities/management-validation')
 
 // Just the Route to build inventory by classification view
 router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId)); //This will get the classificationId and use the method buildByClassificationId from the object invController.
 
 //Route for each car based on inventory id, this will be used by buildInventoryID to get the parameter
 router.get("/detail/:inventoryId", utilities.handleErrors(invController.buildByInventoryId));
+
+/** Routes for management */
+router.get("/", utilities.validateAccountType, utilities.handleErrors(invController.buildManagement));
+
+router.get("/go-to-invmgmt", utilities.validateAccountType, utilities.handleErrors(invController.buildManagement));
+
+/** Route for add new classification */
+router.get("/add-classification", utilities.validateAccountType, utilities.handleErrors(invController.buildNewClassification));
+
+/** Route for add new inventory */
+router.get("/add-inventory", utilities.validateAccountType, utilities.handleErrors(invController.buildNewInventory));
+
+/** Post to add new classification */
+router.post(
+    "/add-classification",
+    mgmtValidate.newClassificationRules(),
+    mgmtValidate.checkClassificationData,
+    utilities.handleErrors(invController.registerNewClassification)
+);
+
+/** Post to add new inventory element */
+router.post(
+    "/add-inventory",
+    mgmtValidate.newInventoryRules(),
+    mgmtValidate.checkInventoryData,
+    utilities.handleErrors(invController.registerNewInventory)
+);
+
+/** This is to display the list of items */
+router.get("/getInventory/:classification_id", utilities.handleErrors(invController.getInventoryJSON))
+
+/** Get the inventory ID to Modify/Edit an item */
+router.get("/edit/:inventory_id", utilities.validateAccountType, utilities.handleErrors(invController.buildEditView))
+
+/** Modify/Edit an item */
+router.post("/update/", 
+    mgmtValidate.newInventoryRules(),
+    mgmtValidate.checkUpdateData,
+    utilities.handleErrors(invController.updateInventory))
+
+/** Get the inventory ID to Delete an item */
+router.get("/delete/:inventory_id", utilities.validateAccountType, utilities.handleErrors(invController.buildDeleteView))
+
+/** Process the deletion of an item */
+router.post("/delete/", 
+    utilities.handleErrors(invController.deleteInventoryElement))
 
 
 module.exports = router;
